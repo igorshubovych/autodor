aff97cdcee2e40a942aab459fd88bf034b12d1cc
Igor Shubovych <igor.shubovych@gmail.com>
2011-03-14 21:07:02 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Autodor::Application.config.secret_token = 'fa143fb9886b4491736855d7a892051be1bbf1a33797cbad1257bf73dabb492f59c0d12fb29fd3506d37aff91ef77f794a71e1d955f9204e48b331f22d577d80'
