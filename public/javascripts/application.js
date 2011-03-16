1f249e5b424cb93241c233914d6a8175296ad528
shybovycha <shybovycha@gmail.com>
2011-03-16 19:09:35 +0200

    moofoo

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,7 @@
-/*$(document).ready(function() {
+google.load("jquery", "1.5.1");
+google.load("jqueryui", "1.8.10");
+
+$(document).ready(function() {
     var cloudmade = new CM.Tiles.CloudMade.Web({key: 'BC9A493B41014CAABB98F0471D759707'});
     var map = new CM.Map('map', cloudmade);
     map.setCenter(new CM.LatLng(51.514, -0.137), 15);
@@ -6,4 +9,3 @@
 	map.addControl(new CM.ScaleControl());
 	map.addControl(new CM.OverviewMapControl());
 });
-*/
