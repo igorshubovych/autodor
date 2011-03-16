13fe9871dca7e35366129d6686a255f03c015956
shybovycha <shybovycha@gmail.com>
2011-03-16 13:17:05 +0200

    Minor start page changes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$.ready(function() {
     var cloudmade = new CM.Tiles.CloudMade.Web({key: 'BC9A493B41014CAABB98F0471D759707'});
     var map = new CM.Map('map', cloudmade);
     map.setCenter(new CM.LatLng(51.514, -0.137), 15);
