bcb45784e1111fd4979d9f9685ea36effd755e16
shybovycha <shybovycha@gmail.com>
2011-03-16 23:50:24 +0200

    Centering try #1

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,14 @@
+var conn = null, map = null;
+
 $(document).ready(function() {
-    var cloudmade = new CM.Tiles.CloudMade.Web({key: 'BC9A493B41014CAABB98F0471D759707'});
-    var map = new CM.Map('map', cloudmade);
+    conn = new CM.Tiles.CloudMade.Web({key: 'BC9A493B41014CAABB98F0471D759707'});
+    map = new CM.Map('map', conn);
     map.setCenter(new CM.LatLng(51.514, -0.137), 15);
 	map.addControl(new CM.LargeMapControl());
 	map.addControl(new CM.ScaleControl());
 	map.addControl(new CM.OverviewMapControl());
 });
+
+function createMap(lat, lon) {
+	map.setCenter(new CM.LatLng(lat, lon), 17);
+}
