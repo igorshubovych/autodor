1bf3246f0ccacf3bb7484554dc60869cf7239bdc
shybovycha <shybovycha@gmail.com>
2011-03-17 21:27:57 +0200

    Switched to local jquery

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,9 @@ class HomeController < ApplicationController
   	@languages = [ "ru", "ua", "en" ]
   	@current_lang = "ua"
   end
+  
+  def moar
+	@languages = [ "ru", "ua", "en" ]
+  	@current_lang = "ua"
+  end
 end
