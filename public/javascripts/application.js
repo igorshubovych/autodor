bb02c830c17bf957e4e50e1212688cd0f8eec6e9
shybovycha <shybovycha@gmail.com>
2011-03-18 22:41:08 +0200

    trying adding context-menu 4z map

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -123,4 +123,12 @@ $(document).ready(function() {
 			currMarker.setLatLng(pos);
 		}
 	});
+	
+	var menu1 = [ 
+		{'Option 1':function(menuItem,menu) { alert("You clicked Option 1!"); } }, 
+		$.contextMenu.separator, 
+		{'Option 2':function(menuItem,menu) { alert("You clicked Option 2!"); } } 
+	]; 
+
+	$('#map').contextMenu(menu1, { theme: 'xp'} );
 });
