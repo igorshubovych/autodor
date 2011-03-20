a29a29f464eb5a7e1bc2da29504e1a128ab13430
Igor Shubovych <igor.shubovych@gmail.com>
2011-03-20 23:20:13 +0200

    Refactoring layouts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  layout 'map'
+  
   def index
   	@languages = [ "ru", "ua", "en" ]
   	@current_lang = "ua"
@@ -10,17 +12,14 @@ class HomeController < ApplicationController
   end
   
   def weather
-	respond_to do |format|
-		format.js do
-			url = "http://62.244.10.66/gmaps/connector.php?lang=en&type=large&"
-			url += "x1=#{params[:x1]}&x2=#{params[:x2]}&y1=#{params[:y1]}&y2=#{params[:y2]}&zoom=#{params[:zoom]}"
-			
-			res = CurbFu.get(url)
-			render :json => res.body
-		end
-	end
-  end
-    
-  def objects
+	  respond_to do |format|
+  		format.js do
+  			url = "http://62.244.10.66/gmaps/connector.php?lang=en&type=large&"
+  			url += "x1=#{params[:x1]}&x2=#{params[:x2]}&y1=#{params[:y1]}&y2=#{params[:y2]}&zoom=#{params[:zoom]}"
+	
+  			res = CurbFu.get(url)
+  			render :json => res.body
+  		end
+  	end
   end
 end
