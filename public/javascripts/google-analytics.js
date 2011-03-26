074312d6e8fbcccef26b770e8308114fd563b5d9
shybovycha <shybovycha@gmail.com>
2011-03-26 09:45:28 +0200

    fixed one more symbol... sh*t...

--- public/javascripts/google-analytics.js
+++ public/javascripts/google-analytics.js
@@ -8,6 +8,6 @@ _gaq.push(['_trackPageview']);
 	ga.type = 'text/javascript';
 	ga.async = true;
 	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementByTagName('script')[0];
+	var s = document.getElementsByTagName('script')[0];
 	s.parentNode.insertBefore(ga, s);
 })();
