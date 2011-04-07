2ab6e9e482c42f061250e76b3265d4fbaa7e5349
shybovycha <shybovycha@gmail.com>
2011-04-08 02:31:23 +0300

    route type fix, datacenter photo updated, links added

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -11,6 +11,24 @@ class PageController < ApplicationController
   end
   
   def links
+    @links = [ 
+				MLink.new('links.euro2012highway', 'http://euro2012highway.blogspot.com/', 'euro2012highway_thumb.png'),
+				MLink.new('links.diprodor', 'http://diprodor.com', 'diprodor_thumb.png'),
+				MLink.new('links.ukrautodor', 'http://ukravtodor.gov.ua/clients/ukrautodor.nsf', '0.jpg'),
+				MLink.new('links.uefa_com', 'http://www.uefa.com/uefaeuro2012/', '1.jpg'),
+				MLink.new('links.euro-2012', 'http://euro-2012.com.ua/', '2.jpg'),
+				MLink.new('links.2012ua', 'http://2012ua.net/', '3.jpg'),
+			 ]
   end
 
+end
+
+class MLink
+	attr_accessor :title, :url, :img
+
+	def initialize(a, b, c)
+	  self.title = a
+	  self.url = b
+	  self.img = c
+	end
 end
