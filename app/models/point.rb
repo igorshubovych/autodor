5508aaccd9eb6a7428a4fb79ddfd2288aa44f645
Igor Shubovych <igor.shubovych@gmail.com>
2011-04-10 19:52:22 +0300

    Remove model point

--- app/models/point.rb
+++
@@ -1,9 +0,0 @@
-class Point < ActiveRecord::Base
-	def save(upload)
-		filename = upload[:datafile].original_filename
-		dir = 'public/images/uploaded/'
-		
-		path = File.join(dir, filename)
-		File.open(path, 'wb') { |f| f.write(upload['datafile'].read) }
-	end
-end
