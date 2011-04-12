0756ff20c529d3d577f8b2f9f6c480ff5478587a
shybovycha <shybovycha@gmail.com>
2011-04-12 23:18:59 +0300

    marker move updates latlon fields

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -16,8 +16,8 @@ class PageController < ApplicationController
 				MLink.new('links.diprodor', 'http://diprodor.com', 'diprodor_thumb.png'),
 				MLink.new('links.ukrautodor', 'http://ukravtodor.gov.ua/clients/ukrautodor.nsf', '0.jpg'),
 				MLink.new('links.uefa_com', 'http://www.uefa.com/uefaeuro2012/', '1.jpg'),
-				MLink.new('links.euro-2012', 'http://euro-2012.com.ua/', '2.jpg'),
-				MLink.new('links.2012ua', 'http://2012ua.net/', '3.jpg'),
+				MLink.new('links.euro_2012', 'http://euro-2012.com.ua/', '2.jpg'),
+				MLink.new('links._2012ua', 'http://2012ua.net/', '3.jpg'),
 			 ]
   end
 
