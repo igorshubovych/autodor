55212af7f2e92924501bb7fa0140e7486b0af831
Artem Shoobovych <shybovycha@gmail.com>
2011-10-20 22:58:39 +0300

    trying to fix mailing system. configured heroku as tutorial says

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+config.action_mailer.delivery_method = :smtp
+
 # Initialize the rails application
 Autodor::Application.initialize!
