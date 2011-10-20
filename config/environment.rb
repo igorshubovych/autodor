e6807988f5de556b07071e12ebe9a07a23801e0e
Artem Shoobovych <shybovycha@gmail.com>
2011-10-20 23:07:33 +0300

    quick app fix - rack crashed for smtp config as the goddamn heroku tutorial said

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-config.action_mailer.delivery_method = :smtp
-
 # Initialize the rails application
 Autodor::Application.initialize!
